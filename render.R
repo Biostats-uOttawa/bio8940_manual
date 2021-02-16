498aee536071bc88cfee57c61ae4462164241c10
JulienGAMartin <julien.ga.martin@gmail.com>
2021-02-16 17:09:18 -0500

    saving r_obj and caching

--- render.R
+++ render.R
@@ -5,7 +5,7 @@ rm(list = ls())
 bookdown::render_book("index.Rmd", "bookdown::pdf_book", params = list(echo_sol = FALSE))
 rm(list = ls())
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
-bookdown::calibre("docs/Labo_BIO4558.epub", "mobi")
+#bookdown::calibre("docs/Labo_BIO4558.epub", "mobi")
 
 
 # bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
