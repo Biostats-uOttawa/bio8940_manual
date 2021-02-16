dd4dd3546a7060a25770d289d7882861e2d665af
JulienGAMartin <julien.ga.martin@gmail.com>
2021-02-16 12:33:52 -0500

    styler

--- render.R
+++ render.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env Rscript
-rm(list=ls());bookdown::render_book("index.Rmd", "bookdown::gitbook", params = list(echo_sol = TRUE))
-rm(list=ls());bookdown::render_book("index.Rmd", "bookdown::pdf_book", params = list(echo_sol = FALSE))
-rm(list=ls());bookdown::render_book("index.Rmd", 'bookdown::epub_book')
-bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')
+rm(list = ls())
+bookdown::render_book("index.Rmd", "bookdown::gitbook", params = list(echo_sol = TRUE))
+rm(list = ls())
+bookdown::render_book("index.Rmd", "bookdown::pdf_book", params = list(echo_sol = FALSE))
+rm(list = ls())
+bookdown::render_book("index.Rmd", "bookdown::epub_book")
+bookdown::calibre("docs/Labo_BIO4558.epub", "mobi")
 
 
-#bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
+# bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
 bookdown::preview_chapter("01-introR_4.Rmd", output_dir = "docs/preview")
 bookdown::preview_chapter("02-Gpower.Rmd", output_dir = "docs/preview")
 bookdown::preview_chapter("03-reg_lin.Rmd", output_dir = "docs/preview")
